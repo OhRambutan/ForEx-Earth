9d68f90fe5eca63b71a4b0d8cd991d4e65a1cb53
Will <william.g.armstrong@gmail.com>
2013-09-28 16:56:07 -0400

    Remove google code and add some modifications to the glove

--- js/forexearth.js
+++ js/forexearth.js
@@ -1,14 +1,48 @@
-if(!Detector.webgl){
-  Detector.addGetWebGLMessage();
-} else {
+var scene, camera, renderer, controls;
+init();
+animate();
 
-  var years = ['1990','1995','2000'];
-  var container = document.getElementById('globe');
-  var globe = new DAT.Globe(container);
-  console.log(globe);
+function init() {
+	if (Detector.webgl) {
+		renderer = new THREE.WebGLRenderer();
+	} else {
+		Detector.addGetWebGLMessage();
+	}
+	
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 2000);
 
-  TWEEN.start();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0x000000, 1);
+	document.body.appendChild(renderer.domElement);
 
-  globe.createPoints();
-  globe.animate();
+	var geometry = new THREE.SphereGeometry(500,50,50);
+	var texture = THREE.ImageUtils.loadTexture('img/world.jpg');
+
+	var material = new THREE.MeshBasicMaterial({map : texture});
+	var cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+
+	var light = new THREE.PointLight();
+	scene.add(light);
+
+	camera.position.z = 1000;
+
+	controls = new THREE.TrackballControls(camera);
+	controls.minDistance = 600;
+	controls.maxDistance = 1500;
+	controls.domElement = document.body;
+	window.addEventListener('resize', onWindowResize, false);
+}
+
+function animate () {
+	requestAnimationFrame(animate);
+	controls.update(1);
+	renderer.render(scene, camera);
+};
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }

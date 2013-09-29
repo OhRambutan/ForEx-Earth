2c98d59a8a033eb488a2a130383c706cd062fb30
Yasith Vidanaarachchi <yasith@tuxv.net>
2013-09-29 01:30:55 -0400

    Refactor forexearth.js

--- js/forexearth.js
+++ js/forexearth.js
@@ -2,50 +2,52 @@ var scene, camera, renderer, controls;
 var highs;
 
 var Shaders = {
-    'earth' : {
-      uniforms: {
-        'texture': { type: 't', value: null }
-      },
-      vertexShader: [
-        'varying vec3 vNormal;',
-        'varying vec2 vUv;',
-        'void main() {',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-          'vNormal = normalize( normalMatrix * normal );',
-          'vUv = uv;',
-        '}'
-      ].join('\n'),
-      fragmentShader: [
-        'uniform sampler2D texture;',
-        'varying vec3 vNormal;',
-        'varying vec2 vUv;',
-        'void main() {',
-          'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
-          'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
-        '}'
-      ].join('\n')
+  'earth' : {
+    uniforms: {
+      'texture': { type: 't', value: null }
     },
-    'atmosphere' : {
-      uniforms: {},
-      vertexShader: [
-        'varying vec3 vNormal;',
-        'void main() {',
-          'vNormal = normalize( normalMatrix * normal );',
-          'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-        '}'
-      ].join('\n'),
-      fragmentShader: [
-        'varying vec3 vNormal;',
-        'void main() {',
-          'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
-        '}'
-      ].join('\n')
-    }
-  };
+    vertexShader: [
+      'varying vec3 vNormal;',
+      'varying vec2 vUv;',
+      'void main() {',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        'vNormal = normalize( normalMatrix * normal );',
+        'vUv = uv;',
+      '}'
+    ].join('\n'),
+    fragmentShader: [
+      'uniform sampler2D texture;',
+      'varying vec3 vNormal;',
+      'varying vec2 vUv;',
+      'void main() {',
+        'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+        'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+        'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+        'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
+      '}'
+    ].join('\n')
+  },
 
+  'atmosphere' : {
+    uniforms: {},
+    vertexShader: [
+      'varying vec3 vNormal;',
+      'void main() {',
+        'vNormal = normalize( normalMatrix * normal );',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+      '}'
+    ].join('\n'),
+    fragmentShader: [
+      'varying vec3 vNormal;',
+      'void main() {',
+        'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
+        'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+      '}'
+    ].join('\n')
+  }
+};
+
+var EARTH_RADIUS = 200;
 
 function init() {
 	if (Detector.webgl) {
@@ -61,9 +63,8 @@ function init() {
 	renderer.setClearColor(0x000000, 1);
 	document.body.appendChild(renderer.domElement);
 
-	var geometry = new THREE.SphereGeometry(200,40,30);
+	var geometry = new THREE.SphereGeometry(EARTH_RADIUS, 40, 30);
 	var texture = THREE.ImageUtils.loadTexture('img/world.jpg');
-	//var material = new THREE.MeshBasicMaterial({map : texture});
 	shader = Shaders['earth'];
 	uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
@@ -108,40 +109,11 @@ function addLines(coords) {
 	var count = 0;
 	var SCALE = 0.0001;
 
-
-	/*for (var i = 0; i < coords.length; i++) {
-		var x = Math.floor(coords[i].X * SCALE);
-		var y = Math.floor(coords[i].Y * SCALE);
-		var z = Math.floor(coords[i].Z * SCALE);
-		console.log(x);
-		console.log(y);
-		console.log(z);
-		positions[count] = 0;
-		positions[count + 1] = 0;
-		positions[count + 2] = 0;
-		positions[count + 3] = x;
-		positions[count + 4] = y;
-		positions[count + 5] = z;
-		count += 6;
-	}*/
 	for (var i = 0; i < coords.length; i++) {
 		var lat = coords[i].lat;
 		var lon = coords[i].lon;
-		var phi = (lat - 90) * Math.PI / 180;
-   		var theta = (180 - (lon)) * Math.PI / 180;
-		var x = 400 * Math.sin(phi) * Math.cos(theta);
-		var y = 400 * Math.cos(phi);
-		var z = 400 * Math.sin(phi) * Math.sin(theta);
-		console.log(x);
-		console.log(y);
-		console.log(z);
-		positions[count] = 0;
-		positions[count + 1] = 0;
-		positions[count + 2] = 0;
-		positions[count + 3] = x;
-		positions[count + 4] = y;
-		positions[count + 5] = z;
-		count += 6;
+    positions = Float32Concat(positions, getCoords(lat, lon, 1.0));
+    positions = Float32Concat(positions, getCoords(lat, lon, 1.5));
 	}
 
 	geometry.computeBoundingSphere();
@@ -159,4 +131,46 @@ function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+/**
+ * offset = 1.0 sets the point on the surface of earth.
+ *        = 2.0 sets the point EARTH_RADIUS away from surface of earth.
+ *
+ * returns a list [x, y, z]
+ */
+function getCoords(latitude, longitude, offset) {
+  var phi = (latitude - 90) * Math.PI / 180;  
+  var theta = (180 - longitude) * Math.PI / 180;
+
+  var x = offset * EARTH_RADIUS * Math.sin(phi) * Math.cos(theta);
+  var y = offset * EARTH_RADIUS * Math.cos(phi);
+  var z = offset * EARTH_RADIUS * Math.sin(phi) * Math.sin(theta);
+
+  return [x, y, z]
+}
+
+/**
+ * Takes two points asTHREE.vector3, draws them on the scene
+ */
+function drawLine(pointA, pointB) {
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(pointA);
+  geometry.vertices.push(pointB);
+
+  var material = new THREE.LineBasicMaterial({
+    color: 0x0000ff
+  });
+
+  var line = new THREE.Line(geometry, material);
+}
+
+function Float32Concat(first, second) {
+  var firstLength = first.length;
+  var result = new Float32Array(firstLength + second.length);
+
+  result.set(first);
+  result.set(second, firstLength);
+
+  return result;
 }
